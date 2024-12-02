539e56941dfcfdacb200e2b41eb4bcafcbf243e5
Kijima Daigo <norimaking777@gmail.com>
2024-12-03 01:25:29 +0900

    feat: スクロール対応が楽なListを使うようにする

--- lib/states/main_menu.go
+++ lib/states/main_menu.go
@@ -53,6 +53,7 @@ func (st *MainMenuState) updateMenuContainer() {}
 
 func (st *MainMenuState) initUI() *ebitenui.UI {
 	faceFont := loadFont("ui/JF-Dot-Kappa20B.ttf", 20)
+	buttonImage, _ := loadButtonImage()
 
 	rootContainer := widget.NewContainer(
 		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.RGBA{0x13, 0x1a, 0x22, 0xff})),
@@ -67,9 +68,68 @@ func (st *MainMenuState) initUI() *ebitenui.UI {
 			widget.RowLayoutOpts.Spacing(10), // ボタンの間隔
 		)),
 	)
-	rootContainer.AddChild(st.scenarioSelectButton("坊っちゃん", faceFont, embeds.Bochan))
-	rootContainer.AddChild(st.scenarioSelectButton("吾輩は猫である", faceFont, embeds.WagahaiHaNekoDearu))
-	rootContainer.AddChild(st.scenarioSelectButton("三四郎", faceFont, embeds.Sanshirou))
+
+	entries := []any{}
+	for _, s := range embeds.ScenarioMaster.Scenarios {
+		entries = append(entries, s.Name)
+	}
+	list := widget.NewList(
+		widget.ListOpts.ContainerOpts(widget.ContainerOpts.WidgetOpts(
+			widget.WidgetOpts.MinSize(150, 0),
+			widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+				HorizontalPosition: widget.AnchorLayoutPositionCenter,
+				VerticalPosition:   widget.AnchorLayoutPositionEnd,
+				StretchVertical:    true,
+				Padding:            widget.NewInsetsSimple(50),
+			}),
+		)),
+		widget.ListOpts.Entries(entries),
+		widget.ListOpts.ScrollContainerOpts(
+			widget.ScrollContainerOpts.Image(&widget.ScrollContainerImage{
+				Idle:     image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
+				Disabled: image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
+				Mask:     image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
+			}),
+		),
+		widget.ListOpts.SliderOpts(
+			widget.SliderOpts.Images(&widget.SliderTrackImage{
+				Idle:  image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
+				Hover: image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
+			}, buttonImage),
+			widget.SliderOpts.MinHandleSize(5),
+			widget.SliderOpts.TrackPadding(widget.NewInsetsSimple(2))),
+		widget.ListOpts.HideHorizontalSlider(),
+		widget.ListOpts.EntryFontFace(faceFont),
+		widget.ListOpts.EntryColor(&widget.ListEntryColor{
+			Selected:                   color.NRGBA{R: 0, G: 255, B: 0, A: 255},
+			Unselected:                 color.NRGBA{R: 254, G: 255, B: 255, A: 255},
+			SelectedBackground:         color.NRGBA{R: 130, G: 130, B: 200, A: 255},
+			SelectingBackground:        color.NRGBA{R: 130, G: 130, B: 130, A: 255},
+			SelectingFocusedBackground: color.NRGBA{R: 130, G: 140, B: 170, A: 255},
+			SelectedFocusedBackground:  color.NRGBA{R: 130, G: 130, B: 170, A: 255},
+			FocusedBackground:          color.NRGBA{R: 170, G: 170, B: 180, A: 255},
+			DisabledUnselected:         color.NRGBA{R: 100, G: 100, B: 100, A: 255},
+			DisabledSelected:           color.NRGBA{R: 100, G: 100, B: 100, A: 255},
+			DisabledSelectedBackground: color.NRGBA{R: 100, G: 100, B: 100, A: 255},
+		}),
+		widget.ListOpts.EntryLabelFunc(func(e interface{}) string {
+			key := e.(string)
+			idx := embeds.ScenarioMaster.ScenarioIndex[key]
+			scenario := embeds.ScenarioMaster.Scenarios[idx]
+
+			return scenario.LabelName
+		}),
+		widget.ListOpts.EntryTextPadding(widget.NewInsetsSimple(5)),
+		widget.ListOpts.EntryTextPosition(widget.TextPositionStart, widget.TextPositionCenter),
+		widget.ListOpts.EntrySelectedHandler(func(args *widget.ListEntrySelectedEventArgs) {
+			key := args.Entry.(string)
+			idx := embeds.ScenarioMaster.ScenarioIndex[key]
+			scenario := embeds.ScenarioMaster.Scenarios[idx]
+
+			st.trans = &Transition{Type: TransSwitch, NewStates: []State{&PlayState{scenario: scenario.Body}}}
+		}),
+	)
+	rootContainer.AddChild(list)
 
 	return &ebitenui.UI{Container: rootContainer}
 }
