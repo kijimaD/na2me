dc2100750863b3e0195798fbdefc4ec6c168f277
Kijima Daigo <norimaking777@gmail.com>
2024-12-05 17:06:45 +0900

    fix: リストが入りきってなかったのを直した

--- lib/eui/eui.go
+++ lib/eui/eui.go
@@ -31,7 +31,7 @@ func NewList(listOpts ...widget.ListOpt) *widget.List {
 			widget.ListOpts.EntryFontFace(utils.BodyFont),
 			widget.ListOpts.EntryColor(&widget.ListEntryColor{
 				Selected:                   color.NRGBA{R: 0, G: 255, B: 0, A: 255},
-				Unselected:                 color.NRGBA{R: 254, G: 255, B: 255, A: 255},
+				Unselected:                 color.NRGBA{R: 255, G: 255, B: 255, A: 255},
 				SelectedBackground:         color.NRGBA{R: 130, G: 130, B: 200, A: 255},
 				SelectingBackground:        color.NRGBA{R: 130, G: 130, B: 130, A: 255},
 				SelectingFocusedBackground: color.NRGBA{R: 130, G: 140, B: 170, A: 255},
@@ -44,13 +44,6 @@ func NewList(listOpts ...widget.ListOpt) *widget.List {
 			widget.ListOpts.EntryLabelFunc(func(e interface{}) string { return "" }),
 			widget.ListOpts.EntryTextPadding(widget.NewInsetsSimple(5)),
 			widget.ListOpts.EntryTextPosition(widget.TextPositionStart, widget.TextPositionCenter),
-			widget.ListOpts.ScrollContainerOpts(
-				widget.ScrollContainerOpts.Image(&widget.ScrollContainerImage{
-					Idle:     image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
-					Disabled: image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
-					Mask:     image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
-				}),
-			),
 			widget.ListOpts.SliderOpts(
 				widget.SliderOpts.Images(&widget.SliderTrackImage{
 					Idle:  image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
@@ -59,8 +52,6 @@ func NewList(listOpts ...widget.ListOpt) *widget.List {
 				widget.SliderOpts.MinHandleSize(5),
 				widget.SliderOpts.TrackPadding(widget.NewInsetsSimple(2)),
 			),
-			widget.ListOpts.HideHorizontalSlider(),
-			widget.ListOpts.EntryFontFace(utils.BodyFont),
 			widget.ListOpts.AllowReselect(),
 		}, listOpts...)...,
 	)
