8480fc2551489666a6bebd713cd421ae471411d2
Kijima Daigo <norimaking777@gmail.com>
2024-12-08 20:56:48 +0900

    feat: 行長の下限もチェックする

--- lib/check/check_test.go
+++ lib/check/check_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// 空白行は無視する
 func TestWarnLongLine(t *testing.T) {
 	buf := bytes.Buffer{}
 	r := strings.NewReader(`hello
@@ -15,16 +16,23 @@ worldxxxxx
 safe
 12345678
 あああああああ
-安全
+警告
+短い
+大丈夫
 警告を受ける文字長
 受けない文字長
+
 `)
-	WarnLongLine(r, &buf, 8, "sample.txt")
+	WarnLineLen(r, &buf, 8, 3, "sample.txt")
 
 	expect := `sample.txt
 Line: 2, Length: 10
   worldxxxxx
-Line: 7, Length: 9
+Line: 6, Length: 2
+  警告
+Line: 7, Length: 2
+  短い
+Line: 9, Length: 9
   警告を受ける文字長
 --------------------------------------------------------------------------------
 `
