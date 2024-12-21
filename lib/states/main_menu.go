4505eda4d31c29c1ea200c66b03f77f13c9ba753
Kijima Daigo <norimaking777@gmail.com>
2024-12-21 13:57:19 +0900

    feat: メインメニューを整理する

--- lib/states/main_menu.go
+++ lib/states/main_menu.go
@@ -2,9 +2,11 @@ package states
 
 import (
 	"fmt"
+	"image/color"
 	"strings"
 
 	"github.com/ebitenui/ebitenui"
+	"github.com/ebitenui/ebitenui/image"
 	"github.com/ebitenui/ebitenui/widget"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
@@ -58,29 +60,105 @@ func (st *MainMenuState) updateMenuContainer() {}
 
 func (st *MainMenuState) initUI() *ebitenui.UI {
 	rootContainer := widget.NewContainer(
+		widget.ContainerOpts.WidgetOpts(widget.WidgetOpts.TrackHover(false)),
 		widget.ContainerOpts.Layout(
 			widget.NewGridLayout(
 				widget.GridLayoutOpts.Columns(1),
-				widget.GridLayoutOpts.Spacing(4, 4),
-				widget.GridLayoutOpts.Stretch([]bool{true}, []bool{true}),
+				widget.GridLayoutOpts.Stretch([]bool{true}, []bool{false, true, false}),
 				widget.GridLayoutOpts.Padding(widget.Insets{
-					Top:    4,
-					Bottom: 4,
-					Left:   4,
-					Right:  4,
+					Top:    10,
+					Bottom: 10,
 				}),
+				widget.GridLayoutOpts.Spacing(0, 0),
 			),
 		),
 	)
+
+	footerContainer := widget.NewContainer(widget.ContainerOpts.Layout(widget.NewRowLayout(
+		widget.RowLayoutOpts.Padding(widget.Insets{
+			Left:  25,
+			Right: 25,
+		}),
+	)))
+	footerContainer.AddChild(widget.NewText(
+		widget.TextOpts.Text("github.com/kijimaD/na2me", utils.UIFont, color.NRGBA{100, 100, 100, 255})))
+
+	rootContainer.AddChild(
+		st.headerContainer(),
+		st.bookListContainer(),
+		footerContainer,
+	)
+
+	return &ebitenui.UI{Container: rootContainer}
+}
+
+func (st *MainMenuState) headerContainer() widget.PreferredSizeLocateableWidget {
+	c := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewRowLayout(
+			widget.RowLayoutOpts.Direction(widget.DirectionVertical),
+			widget.RowLayoutOpts.Spacing(10))),
+	)
+
+	c.AddChild(header("話灯機 -- 電子紙芝居方式流通推進委員会",
+		widget.ContainerOpts.WidgetOpts(widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+			Stretch: true,
+		})),
+	))
+
+	c2 := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewRowLayout(
+			widget.RowLayoutOpts.Direction(widget.DirectionVertical),
+			widget.RowLayoutOpts.Padding(widget.Insets{
+				Left:  25,
+				Right: 25,
+			}),
+		)),
+	)
+	c.AddChild(c2)
+
+	c2.AddChild(widget.NewText(
+		widget.TextOpts.Text("注意力散漫たる現代において、歴史的読書方法は競争力を失っている。\n電子紙芝居方式の優れた威力を万人へ宣伝し、方式普及を推進する。", utils.UIFont, color.NRGBA{100, 100, 100, 255})),
+	)
+
+	return c
+}
+
+func header(label string, opts ...widget.ContainerOpt) widget.PreferredSizeLocateableWidget {
+	c := widget.NewContainer(append(opts, []widget.ContainerOpt{
+		widget.ContainerOpts.WidgetOpts(widget.WidgetOpts.TrackHover(false)),
+		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.NRGBA{255, 255, 255, 140})),
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout(widget.AnchorLayoutOpts.Padding(widget.Insets{
+			Top:    4,
+			Bottom: 4,
+			Left:   30,
+			Right:  30,
+		}))),
+	}...)...)
+
+	c.AddChild(widget.NewText(
+		widget.TextOpts.WidgetOpts(widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+			HorizontalPosition: widget.AnchorLayoutPositionStart,
+			VerticalPosition:   widget.AnchorLayoutPositionStart,
+		})),
+		widget.TextOpts.Text(label, utils.UIFont, color.NRGBA{0, 0, 0, 200}),
+		widget.TextOpts.Position(widget.TextPositionStart, widget.TextPositionCenter),
+	))
+
+	return c
+}
+
+func (st *MainMenuState) bookListContainer() *widget.Container {
+	entries := []any{}
+	for _, s := range embeds.ScenarioMaster.Scenarios {
+		entries = append(entries, s.ID)
+	}
+
 	listContainer := widget.NewContainer(
 		widget.ContainerOpts.Layout(
 			widget.NewAnchorLayout(),
 		),
 	)
-	entries := []any{}
-	for _, s := range embeds.ScenarioMaster.Scenarios {
-		entries = append(entries, s.ID)
-	}
+
 	list := eui.NewList(
 		widget.ListOpts.Entries(entries),
 		widget.ListOpts.EntryLabelFunc(func(e interface{}) string {
@@ -98,8 +176,8 @@ func (st *MainMenuState) initUI() *ebitenui.UI {
 			st.trans = &Transition{Type: TransSwitch, NewStates: []State{&PlayState{scenario: scenario}}}
 		}),
 	)
-	listContainer.AddChild(list)
-	rootContainer.AddChild(listContainer)
 
-	return &ebitenui.UI{Container: rootContainer}
+	listContainer.AddChild(list)
+
+	return listContainer
 }
